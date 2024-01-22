c0292cc523b808c71503061df56a9ca524838214
krishnanjalivu <vukrishnanjali@gmail.com>
2024-01-22 23:19:54 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import process from 'process'
+
 
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={process.env.REACT_KEY}>
+  <GoogleOAuthProvider clientId={import.meta.env.REACT_KEY}>
  <BrowserRouter>
 
   <App />
